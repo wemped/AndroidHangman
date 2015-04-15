18b1cf87efa57d923dff6a88b51c844c23cf921b
Drake Wempe <drakewempe@gmail.com>
2015-04-15 13:15:42 -0700

    Added changing of the hangman image, updating the question mark text when guessed correctly, adding characters used, new game button functionality. Still need win and lose game.

--- app/src/main/java/com/drakewempe/hangmandrakewempe/MainActivity.java
+++ app/src/main/java/com/drakewempe/hangmandrakewempe/MainActivity.java
@@ -1,12 +1,20 @@
 package com.drakewempe.hangmandrakewempe;
 
 import android.content.res.Resources;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 
@@ -16,22 +24,111 @@ public class MainActivity extends ActionBarActivity {
     Resources res;
     Random rand;
     String currentAnswer;
-    TextView answer;
+    EditText guessField;
+    TextView answer,guessesUsed,usedChars;
+    char[] answerStatus;
+    ImageView hangmanImageView;
+    int guesses = 6,hangmanImageIndex = 0, dictLength;
+    Button guessButton,resetButton;
+    int[] hangmanImages = {R.drawable.hangman6,R.drawable.hangman5,R.drawable.hangman4,R.drawable.hangman3,R.drawable.hangman2,R.drawable.hangman1,R.drawable.hangman0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        answer.findViewById(R.id.answerText);
+        answer = (TextView)findViewById(R.id.answerText);
+        guessesUsed = (TextView)findViewById(R.id.guessesUsed);
+        guessButton = (Button)findViewById(R.id.guessButton);
+        resetButton = (Button)findViewById(R.id.newGameButton);
+        guessField = (EditText)findViewById(R.id.guessField);
+        usedChars = (TextView)findViewById(R.id.used);
+        hangmanImageView = (ImageView)findViewById(R.id.hangmanImageView);
 
+        //Load the dictionary and add first answer
         rand = new Random();
-
         res = getResources();
         dictionary = res.getStringArray(R.array.dictionary);
-        int dictLength = dictionary.length;
-
+        dictLength = dictionary.length;
         currentAnswer = dictionary[rand.nextInt(dictLength-0+1)+0];
-        answer.setText(currentAnswer);
+        final String questionMarks = new String(new char[currentAnswer.length()]).replace('\0','?');
+        answerStatus = questionMarks.toCharArray();
+
+
+        //DEBUG
+        guessesUsed.setText(currentAnswer);
+        //DEBUG
+
+        answer.setText(questionMarks);
+        //Load hangman images into an array
+        //hangmanImages = getResources().getIntArray(R.array.hangmanImages);
+
+
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentGuess = guessField.getText().toString();
+                if (currentGuess.length() != 1){
+                    Toast.makeText(getApplicationContext(), "Enter a single letter!",
+                        Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (currentAnswer.contains(currentGuess)){
+                    int j = 0;
+                    j = currentAnswer.indexOf(currentGuess,j);
+                    Log.d("j", Integer.toString(j));
+                    while(j!=-1){
+                        //Log.v("debug","yo");
+                        answerStatus[j] = currentGuess.charAt(0);
+                        Log.v("answerStatus", answerStatus.toString());
+                        j = currentAnswer.indexOf(currentGuess,j+1);
+                    }
+                    answer.setText(String.valueOf(answerStatus));
+                    usedChars.append(currentGuess);
+
+                }else{
+                    usedChars.append(currentGuess);
+                    if (hangmanImageIndex == 6){
+                        //youlose
+                    }
+                    hangmanImageIndex++;
+                    hangmanImageView.setImageResource(hangmanImages[hangmanImageIndex]);
+
+
+                }
+                guessField.setText("");
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Load the dictionary and add first answer
+                //rand = new Random();
+                //res = getResources();
+                //dictionary = res.getStringArray(R.array.dictionary);
+                //int dictLength = dictionary.length;
+                currentAnswer = dictionary[rand.nextInt(dictLength-0+1)+0];
+                final String questionMarks = new String(new char[currentAnswer.length()]).replace('\0','?');
+                answerStatus = questionMarks.toCharArray();
+
+
+                //DEBUG
+                guessesUsed.setText(currentAnswer);
+                //DEBUG
+
+                answer.setText(questionMarks);
+
+                hangmanImageIndex = 0;
+                hangmanImageView.setImageResource(hangmanImages[hangmanImageIndex]);
+
+                usedChars.setText("");
+                guessesUsed.setText(R.string.guesses_used);
+
+                //Load hangman images into an array
+                //hangmanImages = getResources().getIntArray(R.array.hangmanImages);
+            }
+        });
+
 
 
 
